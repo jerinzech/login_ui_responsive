27b3514e95c278a20ab46a52dff3819ee4fb1e3a
jerinzech <jerinzjames@gmail.com>
2022-08-18 07:24:38 +0530

    changed overall widget structure

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -13,7 +13,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
-    // double width = MediaQuery.of(context).size.width;
+    double width = MediaQuery.of(context).size.width;
 
     return Expanded(
         child: Container(
@@ -22,6 +22,84 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                    height: height * 0.2,
+                    child: Container(
+                      color: Colors.amber,
+                    )),
+                SizedBox(
+                    height: height * 0.6,
+                    width: width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: width,
+                          padding: EdgeInsets.fromLTRB(width * 0.05, 20, 0, 10),
+                          color: Colors.pink,
+                          child: RichText(
+                              text: TextSpan(children: [
+                            TextSpan(
+                              text: "Let's",
+                              style: ralewayStyle.copyWith(
+                                  fontSize: 25.0,
+                                  fontWeight: FontWeight.normal,
+                                  color: AppColors.blueDarkColor),
+                            ),
+                            TextSpan(
+                              text: " Sign in!",
+                              style: ralewayStyle.copyWith(
+                                  fontSize: 25.0,
+                                  fontWeight: FontWeight.w800,
+                                  color: AppColors.blueDarkColor),
+                            ),
+                          ])),
+                        ),
+                        Container(
+                          width: width,
+                          padding: EdgeInsets.fromLTRB(width * 0.05, 10, 0, 20),
+                          color: Colors.pink,
+                          child: RichText(
+                            text: TextSpan(
+                              text:
+                                  "Hey, please enter the details to sign in \nto your account",
+                              style: ralewayStyle.copyWith(
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.normal,
+                                  color: AppColors.blueDarkColor),
+                            ),
+                          ),
+                        ),
+                        Container(
+                            width: width,
+                            padding:
+                                EdgeInsets.fromLTRB(width * 0.05, 40, 0, 0),
+                            color: Colors.green,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Email",
+                                  style: ralewayStyle.copyWith(
+                                      color: AppColors.blueDarkColor),
+                                ),
+                                Text("Email"),
+                                Text("Email"),
+                                Text("Email"),
+                                Text("Email"),
+                              ],
+                            )),
+                      ],
+                    )),
+                SizedBox(
+                    height: height * 0.2,
+                    child: Container(
+                      color: Colors.amber,
+                    )),
+              ],
             )));
   }
 }
